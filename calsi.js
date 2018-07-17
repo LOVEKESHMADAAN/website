dd17d8b57f5299c77faac9891f5aa76b9f06b099
LOVEKESHMADAAN <33515353+LOVEKESHMADAAN@users.noreply.github.com>
2018-07-16 23:15:07 -0700

    commit
    my

---
+++ calsi.js
@@ -0,0 +1,35 @@
+function btplus()
+{ document.calculator.display.value +="+";
+  document.calculator.display.style.textAlign="right";
+    
+}
+function btmin()
+{ document.calculator.display.value +="-";
+  document.calculator.display.style.textAlign="right";
+    
+}
+function btmult()
+{ document.calculator.display.value +="*";
+  document.calculator.display.style.textAlign="right";
+    
+}
+function btmod()
+{ document.calculator.display.value +="%";
+  document.calculator.display.style.textAlign="right";
+    
+}
+function btdot()
+{ document.calculator.display.value +=".";
+  document.calculator.display.style.textAlign="right";
+    
+}
+function btdiv()
+{ document.calculator.display.value +="/";
+  document.calculator.display.style.textAlign="right";
+    
+}
+function btclear()
+{ document.calculator.display.value ="";
+  document.calculator.display.style.textAlign="right";
+    
+}
